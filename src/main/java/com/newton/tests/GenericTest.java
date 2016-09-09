783c2bd2b70e5c89a28a90bad548436de3decfe2
chhagan <chhaganlal.mathuriya@craftsvilla.com>
2016-09-09 11:04:23 +0530

    before starting extent reporter

--- src/main/java/com/newton/tests/GenericTest.java
+++ src/main/java/com/newton/tests/GenericTest.java
@@ -33,8 +33,8 @@ public class GenericTest extends BaseTest {
 		urlParams = testData.get("URLParams");
 		if (urlParams != null && urlParams.length() > 2) {
 			urlParamsJson = util.readFileAsString("params/" + urlParams + ".json");
-		}else{
-			urlParamsJson="{}";
+		} else {
+			urlParamsJson = "{}";
 		}
 		String actualJson = "";
 
@@ -55,6 +55,7 @@ public class GenericTest extends BaseTest {
 			} else {
 				actualJson = executor.doHttpPost(webResource, ClientResponse.class);
 			}
+
 		}
 
 		}
